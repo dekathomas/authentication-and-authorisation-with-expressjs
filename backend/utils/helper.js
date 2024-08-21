d1722b1ae45057da7c9803aecf73a8373086d1e2
Deka Thomas <33560350+dekathomas@users.noreply.github.com>
2024-08-22 00:29:17 +0700

    init

---
+++ backend/utils/helper.js
@@ -0,0 +1,12 @@
+const get_user_id = async (req) => {
+    return req?.signedCookies?.user?.id || 0;
+}
+
+const get_username = async (req) => {
+    return req?.signedCookies?.user?.username || '';
+}
+
+module.exports = {
+    get_user_id,
+    get_username
+}
